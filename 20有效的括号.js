12302fa8e50c725b24194c5bc6fabf9c3baa12a9
戚生程 <qishengchengg@163.com>
2021-11-26 23:37:52 +0800

    有效的括号

---
+++ 20有效的括号.js
@@ -0,0 +1,20 @@
+function isVaild(item) {
+  if(item.length % 2 !== 0) return false;
+  const stack = [];
+  for (let i = 0; i < item.length; i++) {
+    if(item[i] === '(' || item[i] === '[' || item[i] === '{') {
+      stack.push(item[i]);
+    } else {
+      const max = stack[stack.length - 1];
+      if((max === '(' && item[i] === ')') || (max === '[' && item[i] === ']') || (max === '{' && item[i] === '}')){
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return !stack.length ? true : false;
+  
+}
+
+console.log(isVaild("([(])"));
