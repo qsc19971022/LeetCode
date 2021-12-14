ceece6f75434e01b5f54766b54c3819c7035a0ab
戚生程 <qishengchengg@163.com>
2021-12-14 22:14:33 +0800

    找不同

---
+++ 389找不同.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+ var findTheDifference = function(s, t) {
+  // 取巧方法， 改变了原数据
+  for(let item of s){
+    t = t.replace(item, '');
+  }
+  return t;
+};
